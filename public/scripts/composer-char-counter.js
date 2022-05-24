60040f3f0132e4e63318f60e15988a2c518b1c56
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-23 23:45:36 -0700

    nav, header, new-tweet, counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $('#tweet-text').on('input', function(e) {
+    const counter =  $(this).next().find('output');
+    let length = $(this).val().length;
+    
+    counter.val(140 - length);
+    
+    if (counter.val() < 0) {
+      counter.css('color','red');
+    } else {
+      counter.css('color','black');
+    }
+  });
+});
